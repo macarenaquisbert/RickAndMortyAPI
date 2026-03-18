4dda8ea06618f4ae439293156fb25997f9a8b767
macarenaquisbert <macarenaquisbert@gmail.com>
2026-03-18 13:42:23 -0300

    al precionar Learn more aparece la informacion del personaja con details.html!

--- js/index.js
+++ js/index.js
@@ -1,38 +1,106 @@
-fetch("https://rickandmortyapi.com/api/character")
-  .then(res => res.json())
-  .then(data => {
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
 
-    const container = document.querySelector("#characters");
+// SI ESTÁS EN DETAILS.HTML
+if (document.getElementById("detail-container")) {
 
-    container.innerHTML = "";
+  fetch(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(res => res.json())
+    .then(character => {
 
-    data.results.forEach(character => {
-      container.innerHTML += `
-        <div style="min-width: 300px;">
-          <div class="card shadow-sm">
+      const container = document.getElementById("detail-container");
 
-            <img src="${character.image}" class="card-img-top">
+      container.innerHTML = `
+        <div class="row align-items-center">
 
-            <div class="card-body">
-              <h5>${character.name}</h5>
-
-              <p class="mb-1">Gender: ${character.gender}</p>
-              <p class="mb-1">Status: ${character.status}</p>
-              <p>Species: ${character.species}</p>
-
-              <div class="d-flex justify-content-between mt-3">
-                <button class="btn btn-outline-primary">Learn more!</button>
-
-                <button class="btn btn-outline-warning">
-                  <i class="fa-regular fa-heart"></i>
-                </button>
-              </div>
-            </div>
-
+          <div class="col-md-6">
+            <img src="${character.image}" class="img-fluid rounded">
           </div>
+
+          <div class="col-md-6 text-center">
+            <h1>${character.name}</h1>
+
+            <p class="mt-3">
+              Este personaje está ${character.status} y pertenece a la especie ${character.species}.
+            </p>
+          </div>
+
+        </div>
+
+        <hr class="my-4">
+
+        <div class="row text-center text-danger fw-bold">
+
+          <div class="col">
+            <p>Name</p>
+            <p>${character.name}</p>
+          </div>
+
+          <div class="col">
+            <p>Status</p>
+            <p>${character.status}</p>
+          </div>
+
+          <div class="col">
+            <p>Gender</p>
+            <p>${character.gender}</p>
+          </div>
+
+          <div class="col">
+            <p>Species</p>
+            <p>${character.species}</p>
+          </div>
+
         </div>
       `;
     });
 
-  })
-  .catch(err => console.log(err));
+}
+
+
+//  SI ESTÁS EN INDEX.HTML
+if (document.getElementById("characters")) {
+
+  fetch("https://rickandmortyapi.com/api/character")
+    .then(res => res.json())
+    .then(data => {
+
+      const container = document.querySelector("#characters");
+      container.innerHTML = "";
+
+      data.results.forEach(character => {
+        container.innerHTML += `
+          <div style="min-width: 300px;">
+            <div class="card shadow-sm">
+
+              <img src="${character.image}" class="card-img-top">
+
+              <div class="card-body">
+                <h5>${character.name}</h5>
+
+                <p class="mb-1">Gender: ${character.gender}</p>
+                <p class="mb-1">Status: ${character.status}</p>
+                <p>Species: ${character.species}</p>
+
+                <div class="d-flex justify-content-between mt-3">
+
+                  <!-- LINK A DETAILS -->
+                  <a href="details.html?id=${character.id}" class="btn btn-outline-primary">
+                    Learn more!
+                  </a>
+
+                  <button class="btn btn-outline-warning">
+                    <i class="fa-regular fa-heart"></i>
+                  </button>
+
+                </div>
+              </div>
+
+            </div>
+          </div>
+        `;
+      });
+
+    })
+    .catch(err => console.log(err));
+}
